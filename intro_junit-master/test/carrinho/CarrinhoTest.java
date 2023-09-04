9b4fdb75cfc935ddc9395b0633def86581a7caa7
Natan Bueno <72169218+Natan-Bueno@users.noreply.github.com>
2023-09-04 13:54:54 -0300

    test_add_itens_carrinho_erro

--- intro_junit-master/test/carrinho/CarrinhoTest.java
+++ intro_junit-master/test/carrinho/CarrinhoTest.java
@@ -13,3 +13,12 @@ public class CarrinhoTest {
     public void inicializa() {
         carrinho = new Carrinho();
     }
+
+    @DisplayName("Testa adicionar itens ao carrinho")
+    @Test
+    public void testAddItem() {
+        carrinho.addItem(new Produto("Camisa", 10.00));
+        int qtd = carrinho.getQtdeItems();
+        Assertions.assertEquals(-1, qtd);
+    }
+}
